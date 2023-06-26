3bd3ba4de6138ca6ba56afdd734d250d555f7c33
onja <onjaasz@gmail.com>
2023-06-26 14:29:00 +0200

    modif contenu helloworld

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -4,5 +4,5 @@ import groovy.json.* //importe toutes les lib groovy nécessaires
 
 def 	call(body) { //declare une fonction qui récupère un call de la fonction 'hello World'. body récupère les variables
 
-	println("Hello Wold!!!") //afficher un 'hello world'
+	println("Manaona ianareo mianakavy") //afficher un 'hello world'
 }
