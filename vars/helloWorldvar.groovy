c6c776eefac651514c8b6128968f6b2f4b7f0fb7
onja <onjaasz@gmail.com>
2023-06-26 15:08:06 +0200

    add variables

--- vars/helloWorldvar.groovy
+++ vars/helloWorldvar.groovy
@@ -3,6 +3,13 @@
 import groovy.json.* //importe toutes les lib groovy nécessaires
 
 def 	call(body) { //declare une fonction qui récupère un call de la fonction 'hello World'. body récupère les variables
+	def mapVars = [:] //sorte de hash qui permet de récupérer ou stocker les clés-valeurs
+	body.resolveStrategy = Closure.DELEGATE_FIRST //récupérer le body et de l'intégrer dans la map : mapVars
+	body.delegate = mapVars
+	body ()
 
-	println("Manaona ianareo mianakavy") //afficher un 'hello world'
+	def name = maVars.name //récupère la valeur et l'insère dans name
+	def action = maVars.action //récupère la valeur et l'insère dans name/
+
+	println ( action + " " + name + "!!")
 }
