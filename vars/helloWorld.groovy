259fa2fa4d8df9f74ac481364f3605736d2de5ec
onja <onjaasz@gmail.com>
2023-06-26 14:14:10 +0200

    fist init

---
+++ vars/helloWorld.groovy
@@ -0,0 +1,8 @@
+#!/bin/env groovy //on utilise le langage groovy
+
+import groovy.json.* //importe toutes les lib groovy nécessaires
+
+def 	call(body) { //declare une fonction qui récupère un call de la fonction 'hello World'. body récupère les variables
+
+	println("Hello Wold!!!") //afficher un 'hello world'
+}
